1713b545d6b7dc298d5b21c48c3cf146c6498ed0
AkosBakos <88320884+DIGIT-BS@users.noreply.github.com>
2021-09-22 12:50:17 +0200

    Update SetupComplete.cmd

--- Profiles/GYMKG-SD.ps1
+++ Profiles/GYMKG-SD.ps1
@@ -125,10 +125,8 @@ $AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\Autopilot.cmd' -Encoding
 #================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-REM Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-MD C:\Akos_Setupcomplete
+RD C:\OSDCloud\OS /S /Q
+RD C:\Drivers /S /Q
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
