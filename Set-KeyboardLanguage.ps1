2fde3a1af7b399b4c918a0c835ade094d76d6114
DIGIT-BS <88320884+DIGIT-BS@users.noreply.github.com>
2021-10-04 10:09:47 +0200

    Remove Script Transcript Part

--- Set-KeyboardLanguage.ps1
+++ Set-KeyboardLanguage.ps1
@@ -1,9 +1,4 @@
-Start-Transcript -Path %TEMP%\Set-KeyboardLanguage.txt
-Write-Host -ForegroundColor Green "Set keyboard language for de-CH"
-
 $LanguageList = Get-WinUserLanguageList
-Write-Host -ForegroundColor Green "Keyboard list before change"
-Write-Host $LanguageList
 
 $LanguageList.Add("de-CH")
 Set-WinUserLanguageList $LanguageList -Force
@@ -15,7 +10,3 @@ $LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'de-DE'))
 Set-WinUserLanguageList $LanguageList -Force
 
 $LanguageList = Get-WinUserLanguageList
-Write-Host -ForegroundColor Green "Keyboard list after change"
-Write-Host $LanguageList
-
-Stop-Transcript
