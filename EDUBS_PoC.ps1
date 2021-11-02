1bf9d9d7ee231f941882d2fa857928a24c261cba
DIGIT-BS <88320884+DIGIT-BS@users.noreply.github.com>
2021-11-02 08:38:07 +0100

    Remove module installations

--- EDUBS_PoC.ps1
+++ EDUBS_PoC.ps1
@@ -1,20 +1,14 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
-
 #Make sure I have the latest OSD Content
-Write-Host ""
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Update-Module OSD -Force
+#Write-Host ""
+#Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+#Update-Module OSD -Force
 
-Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-Write-Host ""
+#Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+#Import-Module OSD -Force
+#Write-Host ""
 
 #Start OSDCloudScriptPad - to choose bertween the different profiles
 Write-Host -ForegroundColor Red "Start OSDPad"
