3f0da9347b7ca5a30e237cc22591d44b83fa734b
AkosBakos <helpdesk@mme.ch>
2021-12-01 09:05:58 +0100

    Cleanup Script

--- EDUBS_PoC.ps1
+++ EDUBS_PoC.ps1
@@ -1,15 +1,6 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-#Make sure I have the latest OSD Content
-#Write-Host ""
-#Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-#Update-Module OSD -Force
-
-#Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-#Import-Module OSD -Force
-#Write-Host ""
-
 #Start OSDCloudScriptPad - to choose bertween the different profiles
 Write-Host ""
 Write-Host -ForegroundColor Green "Start OSDPad"
